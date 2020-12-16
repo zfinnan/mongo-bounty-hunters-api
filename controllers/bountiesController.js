8aaa6e83948abd5e081a5579c3d41e592d043557
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-16 11:30:49 -0500

    change to port 3001

--- controllers/bountiesController.js
+++ controllers/bountiesController.js
@@ -3,32 +3,53 @@ const router = require('express').Router()
 const models = require('../models')
 
 router.get('/', (req, res) => {
-    models.Bounty.find().then((foundBounties) => {
-        res.status(200).json({ bounties: foundBounties })
-    })
-    .catch((err) => { res.send(err) })
-})
+  models.Bounty.find().then((foundBounties) => {
+    res.status(200).json({ bounties: foundBounties })
+  })
+  .catch((error) => res.send({ error }))})
 
 router.get('/:id', (req, res) => {
-    models.Bounty.findOne({_id: req.params.id}).then((bounty) => {
-        res.status(200).json({ bounty })
-    })
-    .catch((err) => { res.send(err) })
-})
+  models.Bounty.findOne({_id: req.params.id}).then((bounty) => {
+    res.status(200).json({ bounty })
+  })
+  .catch((error) => res.send({ error }))})
 
 router.post('/', (req, res) => {
-    models.Bounty.create(req.body).then((bounty) => {
-        res.status(201).json({ bounty })
-    })
-    .catch((err) => { res.send(err) })
-})
+  models.Bounty.create(req.body).then((bounty) => {
+    res.status(201).json({ bounty })
+  })
+  .catch((error) => res.send({ error }))})
 
 router.put('/:id', (req, res) => {
-    res.send('hello from PUT /bounties/:id')
+  // const wantedFor = req.body.wantedFor
+  // const client = req.body.client
+  // const reward = req.body.reward
+  // const hunters = req.body.hunters
+  // const captured = req.body.captured
+  // const lastSeen = req.body.lastSeen
+  
+  const { wantedFor, client, reward, hunters, captured, lastSeen } = req.body
+  
+  models.Bounty.update({
+    _id: req.params.id
+  }, {$set: {
+    wantedFor,
+    client,
+    reward,
+    hunters,
+    captured,
+    lastSeen
+  }})
+  .then((bounty) => {
+    res.status(201).json({ bounty })
+  })
+  .catch((error) => res.send({ error }))
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('hello from DELETE /bounties/:id')
+  models.Bounty.deleteOne({ _id: req.params.id })
+  .then((bounty) => res.status(201).json({ bounty }))
+  .catch((error) => res.send({ error }))
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.use('/bounties', require('./controllers/bountiesController'))
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 })
